e5bba7e46bf2bcde1e84fddd3e9282fac66cfbc7
Stephen Brinkworth <xoman5144@gmail.com>
2017-01-31 12:12:41 -0700

    initial

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -8,4 +8,53 @@ chai.use( chaiEnzyme );
 
 import List from "./List";
 
-test( "dummy test", t => t.pass() );
+test( "List removes item from toDos when deleteToDo is called", () => {
+  const list = shallow(
+    <List />
+  );
+
+  list.setState({toDos: [{ text: 'thing1', complete: false }, { text: 'thing2', complete: false }]});
+
+  list.instance().deleteToDo({text: 'thing2'});
+
+  expect( list.state().toDos.length ).to.eql(1);
+});
+
+test( "List does not remove incorrect item from toDos when deleteToDo is called", () => {
+  const list = shallow(
+    <List />
+  );
+
+  list.setState({toDos: [{ text: 'thing1' }, { text: 'thing2' }]});
+
+  list.instance().deleteToDo({text: 'thing3'});
+
+  expect( list.state().toDos.length ).to.eql(2);
+});
+
+test( "List adds items to toDos", () => {
+  const list = shallow(
+    <List />
+  );
+
+  list.instance().submitToDo('thing1');
+
+  expect( list.state().toDos.length ).to.eql(1);
+  expect( list.state().toDos[0] ).to.eql({text: 'thing1', complete: false});
+});
+
+test( "List toggles matching toDos complete value", () => {
+  const list = shallow(
+    <List />
+  );
+
+  list.setState({toDos: [{ text: 'thing1', complete: false }, { text: 'thing2', complete: false }]});
+
+  list.instance().toggleCompletion({text: 'thing2'});
+
+  expect( list.state().toDos ).to.eql([{ text: 'thing1', complete: false }, { text: 'thing2', complete: true }]);
+
+  list.instance().toggleCompletion({text: 'thing2'});
+
+  expect( list.state().toDos ).to.eql([{ text: 'thing1', complete: false }, { text: 'thing2', complete: false }]);
+});
--- src/components/List/NewToDo/NewToDo.test.js
+++ src/components/List/NewToDo/NewToDo.test.js
@@ -3,9 +3,36 @@ import test from "ava";
 import { shallow } from "enzyme";
 import chai, { expect } from "chai";
 import chaiEnzyme from "chai-enzyme";
+import sinon from "sinon";
 
 chai.use( chaiEnzyme );
 
 import NewToDo from "./NewToDo";
 
-test( "dummy test", t => t.pass() );
+test( "NewToDo calls props.submit on form submition", () => {
+  const submitSpy = sinon.spy();
+
+  const newToDo = shallow(
+    <NewToDo submit={ submitSpy } />
+  );
+
+  newToDo
+    .find( '.new-to-do' )
+    .simulate('submit', { preventDefault() {} });
+
+    sinon.assert.calledOnce( submitSpy )
+});
+
+test( "NewToDo calls handleChange on input change", () => {
+
+  const newToDo = shallow(
+    <NewToDo submit={ () => null } />
+  );
+
+  newToDo
+    .find( '.new-to-do__input' )
+    .simulate('change', { target: { value: 'test' } });
+
+  expect( newToDo.state().toDo ).to.eql('test');
+
+});
